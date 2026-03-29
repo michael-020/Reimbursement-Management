6820eaae9173186951098c3a3d0b19bf4fd34bfc
Parth2684 <bhosle6006@gmail.com>
2026-03-29 11:19:35 +0530

    feat: add prisma singlegton and remove seed

--- app/api/(auth)/signup/route.ts
+++ app/api/(auth)/signup/route.ts
@@ -1,13 +1,27 @@
-import { NextRequest, NextResponse } from 'next/server';
-import z from "zod"
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+
 
 const SignupSchema = z.object({
   name: z.string(),
   email: z.string(),
   password: z.string(),
-  
-})
+  country: z.string(),
+  currency: z.string()
+});
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
+  const body = await req.json();
+  const parsedBody = SignupSchema.safeParse(body);
+  if (!parsedBody.success) {
+    return NextResponse.json(
+      {
+        message: parsedBody.error.cause,
+      },
+      { status: 411 },
+    );
+  }
   
-}
+  
+  
+};
