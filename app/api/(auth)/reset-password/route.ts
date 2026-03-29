4e207ba31afe5aef3f8ab3bec5f998af48806c9b
Parth2684 <bhosle6006@gmail.com>
2026-03-29 12:59:30 +0530

    feat: add users and forgot password route added

--- app/api/(auth)/reset-password/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextRequest } from 'next/server';
-import { auth } from '../../../../actions/authAction';
-
-
-
-export const POST = async (req: NextRequest) => {
-  const user = await auth()
-  
-}
