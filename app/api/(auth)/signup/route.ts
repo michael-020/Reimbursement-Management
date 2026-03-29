b63ac415d2fb89118c3962fb7ff4e6215f252c3e
Parth2684 <bhosle6006@gmail.com>
2026-03-29 10:48:15 +0530

    fix: relation fix

--- app/api/(auth)/signup/route.ts
+++ app/api/(auth)/signup/route.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
+import z from "zod"
 
-
+const SignupSchema = z.object({
+  name: z.string(),
+  email: z.string(),
+  password: z.string(),
+  
+})
 
 export const POST = async (req: NextRequest, res: NextResponse) => {
   
